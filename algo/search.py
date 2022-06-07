c0121bfdf4c757d7b66d22f115390a7a15306048
Jonatas Emanuel <jonatassilvaemanuel@gmail.com>
2022-06-06 22:02:16 -0300

    fixed square root algorthm

--- algo/search.py
+++ algo/search.py
@@ -1,10 +1,20 @@
 class Search:
-
+    """_summary_
+    """
     def __init__(self) -> None:
         pass
 
     def square_root(self, find_root, begin=0, end=None):
+        """_summary_
 
+        Args:
+            find_root (_type_): _description_
+            begin (int, optional): _description_. Defaults to 0.
+            end (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
         if end is None:
             end = find_root // 2
 
@@ -20,7 +30,3 @@ class Search:
         else:
             print(f'\n\033[31;1m√{find_root} → No square root\033[m\n')
         return None
-
-    # def show_result(self, res):
-    #     print(f'\n\033[1m√{find_root} → {mid}\033[0m\n')
-
